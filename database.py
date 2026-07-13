6f0ddf63f9cadfb3a91b54bc4c8bc8af4d84d62e
Aishwarya Pradhan <aishwaryapradhan84@gmail.com>
2026-07-13 23:14:04 +0530

    Use Streamlit secrets for Supabase

--- database.py
+++ database.py
@@ -2,9 +2,17 @@ from supabase import create_client
 from datetime import date
 import pandas as pd
 
+import streamlit as st
+from supabase import create_client
 
-SUPABASE_URL= "https://wputoymmevsolfgjrqqh.supabase.co"
-SUPABASE_KEY= "sb_publishable_e3RSmwoWBfRtJEAVHneuFQ_zeZt1SZ7"
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
+
+supabase = create_client(
+    SUPABASE_URL,
+    SUPABASE_KEY
+)
+
 
 supabase = create_client(
     SUPABASE_URL,
